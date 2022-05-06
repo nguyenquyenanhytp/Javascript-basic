b27fb6b3f128fbd8eaafa5c308b619c669825b76
unknown <nguyenquyenanhytp@gmail.com>
2022-05-06 11:05:25 +0700

    function sum

--- chapter 1/demo function/main.js
+++ chapter 1/demo function/main.js
@@ -1,6 +1,8 @@
 //hàm tính tổng sum truyền vào 2 tham số a, b và trả về kq a+b
 function sum (a, b) {
+    var a = 4;
+    var b = 10;
     return a + b;
 }
-var cong = (6+6);
-console.log(cong)
+
+console.log (sum());
