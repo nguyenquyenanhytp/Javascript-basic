52ea5fb679ad7b589a0e1612e951b9504ed9e3b6
unknown <nguyenquyenanhytp@gmail.com>
2022-05-09 00:13:48 +0700

    chapter 3

---
+++ chapter 3/main.js
@@ -0,0 +1,13 @@
+//tạo hàm sum cơ bản
+function sum () {
+    alert(1 + 2);
+}
+//tạo hàm triple
+function triple (num) {
+    var tong = num * 3
+    console.log(tong);
+}
+function say (message) { //tham số message
+    console.log(message);
+}
+say('hello Javascript');// đối số
