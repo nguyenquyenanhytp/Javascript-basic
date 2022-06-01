38c9f8fc531a5bf8fb1edc27eace9fa238dbf985
unknown <nguyenquyenanhytp@gmail.com>
2022-06-01 14:11:02 +0700

    api

---
+++ chapter 15 - fetch/app.js
@@ -0,0 +1,11 @@
+//rest api
+
+var courseApi = 'http://localhost:3000/courses';
+
+fetch(courseApi)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(courses) {
+        console.log(courses);
+    });
